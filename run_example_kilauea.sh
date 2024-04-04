4ef452645270064ce1607fd9a21b1056716af7f7
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-04 19:39:41 +0000

    examples: run_example_kilauea.sh script
    For convenience :)
    
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

---
+++ run_example_kilauea.sh
@@ -0,0 +1,5 @@
+flowy_bin=./build/flowy
+input_file=./examples/KILAUEA2014-2015/input.toml
+asc_file=./examples/KILAUEA2014-2015/test20m.asc
+output_folder=./output_KILAUEA
+$flowy_bin $input_file -a $asc_file -o $output_folder
