1e9c1d7eb6f24dd7849010b092dfa8972cb3797f
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-04 20:44:17 +0000

    examples: improved plotting script with cli arguments
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- examples/plot_thickness_from_asc_file.py
+++ examples/plot_thickness_from_asc_file.py
@@ -1,9 +1,13 @@
+from typing import Optional
+import typer
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 from linecache import getline
 import pyvista as pv
 from dataclasses import dataclass
+
+app = typer.Typer()
 
 
 @dataclass
@@ -17,7 +21,7 @@ class AscFile:
     height_data = np.array([], dtype=float)
 
     def __init__(self, path_to_file: Path):
-        header = [getline(path_to_file, i) for i in range(1, 7)]
+        header = [getline(str(path_to_file), i) for i in range(1, 7)]
         values = [float(h.split(" ")[-1].strip()) for h in header]
         (
             self.cols,
@@ -51,58 +55,32 @@ class AscFile:
         self.height_data[~mask] = replace
 
 
-def plot_height_data_pyvista(
-    path_asc_file_initial: Path,
-    path_asc_file_final: Path,
-    warp_factor=1.0,
-    threshold=0.01,
+@app.command()
+def plot_pyvista(
+    path_asc_file_initial: Path = typer.Argument(help="Path to initial ASC file"),
+    path_asc_file_final: Path = typer.Argument(
+        help="Path to final ASC file after lava emplacement"
+    ),
+    warp_factor: float = typer.Option(
+        1.0,
+        "--warp",
+        min=0.0,
+        max=1.0,
+        help="Factor by which to warp the terrain height.",
+    ),
+    threshold: float = typer.Option(
+        0.01, "--thresh", help="Any elevation smaller than the threshold is not plotted"
+    ),
+    image_outfile: Path = typer.Option(
+        None,
+        "-o",
+        help="Path to the output image file. If not set, an image called image.png will be created.",
+    ),
+    interactive: bool = typer.Option(False, "-i", help="Opens an interactive window"),
 ):
     asc_file_initial = AscFile(path_asc_file_initial)
     asc_file_final = AscFile(path_asc_file_final)
 
-    # asc_file_initial.filter_height_data()
-    # asc_file_final.filter_height_data()
-
-    # x_data = asc_file_initial.x_data()
-    # y_data = asc_file_initial.y_data()
-    # min_height = asc_file_initial.min_height()
-
-    # grid = pv.ImageData()
-    # grid.dimensions = [len(x_data), len(y_data), 1]
-
-    # grid.point_data["height_initial"] = (
-    #     asc_file_initial.height_data.flatten(order="F") - min_height
-    # )
-    # grid.point_data["height_final"] = (
-    #     asc_file_final.height_data.flatten(order="F") - min_height
-    # )
-    # grid.point_data["flow"] = (
-    #     grid.point_data["height_final"] - grid.point_data["height_initial"]
-    # )
-
-    # grid = grid.threshold(asc_file_final.nd + 1, scalars="height_initial")
-
-    # grid_initial = grid.warp_by_scalar("height_initial", warp_factor)
-    # grid_final = grid.warp_by_scalar("height_final", warp_factor).threshold(
-    #     threshold, scalars="flow"
-    # )
-    
-
-    # # Create an unstructured grid from the grid_initial, with a thickness
-    # z_cells = np.linspace(0,1,10)
-
-    # xx = np.repeat(grid.x, len(z_cells), axis=-1)
-    # yy = np.repeat(grid.y, len(z_cells), axis=-1)
-    # zz = np.repeat(grid.z, len(z_cells), axis=-1) - np.cumsum(z_cells).reshape((1, 1, -1))
-
-    # terrain_mesh = pv.StructuredGrid(xx, yy, zz)
-    # terrain_mesh["Elevation"] = zz.ravel(order="F")
-
-    # p = pv.Plotter()
-    # # p.add_mesh(grid_initial, scalars="height_initial", color=True, smooth_shading=True)
-    # p.add_mesh(terrain_mesh, scalars="Elevation", color=True, smooth_shading=True)
-    # # p.add_mesh(grid_final, color="red", opacity=0.8, smooth_shading=True)
-    # p.show()
     x_data = asc_file_initial.x_data()
     y_data = asc_file_initial.y_data()
     height_data = asc_file_initial.height_data
@@ -122,31 +100,60 @@ def plot_height_data_pyvista(
         grid.point_data["height_final"] - grid.point_data["height_initial"]
     )
     grid_initial = grid.warp_by_scalar("height_initial", warp_factor)
-    # For the lava flow 
+    # For the lava flow
     grid_final = grid.warp_by_scalar("height_final", warp_factor).threshold(
         threshold, scalars="flow"
     )
 
-    p = pv.Plotter()
-
     # For the terrain
-    z_cells = np.linspace(0,10, 10)
+    z_cells = np.linspace(0, 10, 10)
     xx = np.repeat(grid_initial.x, len(z_cells), axis=-1)
     yy = np.repeat(grid_initial.y, len(z_cells), axis=-1)
-    zz = np.repeat(grid_initial.z, len(z_cells), axis=-1) - np.cumsum(z_cells).reshape((1, 1, -1))
+    zz = np.repeat(grid_initial.z, len(z_cells), axis=-1) - np.cumsum(z_cells).reshape(
+        (1, 1, -1)
+    )
     mesh = pv.StructuredGrid(xx, yy, zz)
     mesh["Elevation"] = zz.ravel(order="F")
     mesh = mesh.threshold(asc_file_final.nd + 1, scalars="Elevation")
     # opacity_terrain = [0, 0.2, 0.9, 0.6, 0.3]
     # opacity_terrain = "sigmoid_r"
-    opacity_terrain=None
+    opacity_terrain = None
 
-    p.add_mesh(mesh, scalars="Elevation", color=True, smooth_shading=True, opacity=opacity_terrain, cmap='gray')
+    if interactive:
+        p = pv.Plotter(off_screen=False)
+    else:
+        p = pv.Plotter(off_screen=True)
+    p.add_mesh(
+        mesh,
+        scalars="Elevation",
+        color=True,
+        smooth_shading=True,
+        opacity=opacity_terrain,
+        cmap="gray",
+    )
     p.add_mesh(grid_final, color="red", opacity=0.8, smooth_shading=True)
-    p.show()
+
+    if interactive:
+        p.show()
+    if image_outfile is not None:
+        p.screenshot(image_outfile)
+    else:
+        p.screenshot("./image.png")
 
 
-def plot_height_data_pyplot(path_asc_file_initial: Path, path_asc_file_final: Path):
+@app.command()
+def plot_pyplot(
+    path_asc_file_initial: Path = typer.Argument(help="Path to initial ASC file"),
+    path_asc_file_final: Path = typer.Argument(
+        help="Path to final ASC file after lava emplacement"
+    ),
+    image_outfile: Path = typer.Option(
+        None,
+        "-o",
+        help="Path to the output image file. If not set, an image called image.png will be created.",
+    ),
+    interactive: bool = typer.Option(False, "-i", help="Opens an interactive window"),
+):
     asc_file_initial = AscFile(path_asc_file_initial)
     asc_file_final = AscFile(path_asc_file_final)
 
@@ -161,12 +168,13 @@ def plot_height_data_pyplot(path_asc_file_initial: Path, path_asc_file_final: Pa
     plt.contour(
         x_data, y_data, asc_file_final.height_data - asc_file_initial.height_data
     )
-
-    plt.show()
+    if interactive:
+        plt.show()
+    if image_outfile is not None:
+        plt.savefig(image_outfile, dpi=300)
+    else:
+        plt.savefig("./image.png")
 
 
 if __name__ == "__main__":
-    path_asc_file_initial = "output_hawaii/initial.asc"
-    path_asc_file_final = "output_hawaii/output.asc"
-    plot_height_data_pyvista(path_asc_file_initial, path_asc_file_final)
-    plot_height_data_pyplot(path_asc_file_initial, path_asc_file_final)
+    app()
