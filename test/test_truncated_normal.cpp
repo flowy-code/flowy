5684f8df9ecd2bcace330e04876c55a173037f1e
Moritz Sallermann <moritzsallermann@gmail.com>
2024-04-20 16:26:41 +0000

    test_truncated_normal: Some actual test directives
    Also hooked it up in meson.build
    
    Co-authored-by: Amrita Goswami <amrita16thaug646@gmail.com>

--- test/test_truncated_normal.cpp
+++ test/test_truncated_normal.cpp
@@ -4,47 +4,53 @@
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <catch2/matchers/catch_matchers_range_equals.hpp>
 
+#include "flowy/include/definitions.hpp"
 #include "flowy/include/math.hpp"
 #include "pdf_cpplib/include/probability_dist.hpp"
+#include "xtensor/xbuilder.hpp"
+#include "xtensor/xmath.hpp"
 #include <fmt/ostream.h>
-#include <algorithm>
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <random>
 namespace fs = std::filesystem;
 
-template<typename T, std::size_t N>
-std::ostream & operator<<( std::ostream & os, std::array<T, N> const & v1 )
-{
-    std::for_each( begin( v1 ), end( v1 ), [&os]( T val ) { os << val << " "; } );
-    return os;
-}
-
 // Samples the distribution n_samples times and writes results to file
-template<typename distT>
-void write_results_to_file( int N_Samples, distT dist, const std::string & filename )
+auto write_results_to_file( const Flowy::VectorX & samples, const std::string & filename )
 {
     auto proj_root_path = fs::current_path();
     auto file           = proj_root_path / fs::path( "test/output_probability_distributions/" + filename );
     fmt::print( "file = {}\n", fmt::streamed( file ) );
     fs::create_directories( file.parent_path() );
 
-    auto gen = std::mt19937( 0 );
-
     std::ofstream filestream( file );
     filestream << std::setprecision( 16 );
 
-    for( int i = 0; i < N_Samples; i++ )
+    for( int i = 0; i < samples.size(); i++ )
     {
-        filestream << dist( gen ) << "\n";
+        filestream << samples[i] << "\n";
     }
     filestream.close();
+    return samples;
 }
 
 TEST_CASE( "Test the probability distributions", "[prob]" )
 {
-    write_results_to_file(
-        10000, ProbabilityDist::truncated_normal_distribution<double>( 0.0, 2, -Flowy::Math::pi, Flowy::Math::pi ),
-        "truncated_normal.txt" );
+    int N_Samples = 100000;
+    auto gen      = std::mt19937( 0 );
+    auto dist = ProbabilityDist::truncated_normal_distribution<double>( 0.0, 2.0, -Flowy::Math::pi, Flowy::Math::pi );
+    Flowy::VectorX samples = xt::empty<double>( { N_Samples } );
+
+    for( int i = 0; i < samples.size(); i++ )
+    {
+        samples[i] = dist( gen );
+    }
+
+    // Use for debugging
+    // write_results_to_file( samples, "truncated_normal.txt" );
+
+    REQUIRE_THAT( xt::mean( samples )(), Catch::Matchers::WithinAbs( 0, 0.01 ) );
+    REQUIRE_THAT( xt::amax( samples )(), Catch::Matchers::WithinAbs( Flowy::Math::pi, 0.01 ) );
+    REQUIRE_THAT( xt::amin( samples )(), Catch::Matchers::WithinAbs( -Flowy::Math::pi, 0.01 ) );
 }
